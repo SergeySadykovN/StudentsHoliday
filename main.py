0666f60dcb14d6eda8dba0b62468d907a9c3fabd
SergeySadykovN <snsadykov@ya.ru>
2024-04-05 13:51:02 +0300

    v.1

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import csv
+
+
 def write_holiday_cities(first_letter):
-    # РЕАЛИЗАЦИЯ
-    pass
+
+
+    # Получить данные из travel_notes.csv & записать в holiday.csv
+    with open('travel-notes.csv', 'r', newline='', encoding='utf8') as file_read, open('holiday.csv', 'w', encoding='utf8') as file_out:
+        # создаем ридер для входного файла
+        reader = csv.reader(file_read)
+
+        # записываем данные в holiday.csv
+        writer = csv.writer(file_out)
+
+        for row in reader:
+            been_list =[]
+            if row[0].startswith(first_letter):
+                been_list.append(row[1])
+                print(been_list)
+
+
+
+letter = "L"
+write = write_holiday_cities(letter)
