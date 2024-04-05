2dff8252014d07361558dd12af9b202699845490
SergeySadykovN <snsadykov@ya.ru>
2024-04-05 15:49:43 +0300

    v.2

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import csv
+import re
+
+travel_notes = []
 
 
 def write_holiday_cities(first_letter):
-
-
+    visited_set = set()
+    visit_set = set()
+    visited_dict = {}
+    visit_dict = {}
     # Получить данные из travel_notes.csv & записать в holiday.csv
-    with open('travel-notes.csv', 'r', newline='', encoding='utf8') as file_read, open('holiday.csv', 'w', encoding='utf8') as file_out:
+    with open('travel-notes.csv', 'r', newline='', encoding='utf8') as file_read, open('holiday.csv', 'w',
+                                                                                       encoding='utf8') as file_out:
         # создаем ридер для входного файла
         reader = csv.reader(file_read)
 
@@ -13,13 +19,24 @@ def write_holiday_cities(first_letter):
         writer = csv.writer(file_out)
 
         for row in reader:
-            been_list =[]
+
             if row[0].startswith(first_letter):
-                been_list.append(row[1])
-                print(been_list)
+                pattern = r"\w+(?:(?:[^;]*\[[^][]*])+[^;]*|[^;']+)"
+                travel_notes.append(row)
+
+                cities = re.findall(pattern, row[1])
+                for city in cities:
+                    visited_set.add(city)
+                visited_dict[row[0]] = cities
+
+                cities = re.findall(pattern, row[2])
+                for city in cities:
+                    visit_set.add(city)
+                visit_dict[row[0]] = cities
+
+        print(visited_dict)
+        print(visit_dict)
 
 
-
-
-letter = "L"
+letter = "A"
 write = write_holiday_cities(letter)
