936837907862f8302eafd90af31c2ca069e70c35
SergeySadykovN <snsadykov@ya.ru>
2024-04-06 11:10:52 +0300

    v.3

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 import csv
 import re
 
-travel_notes = []
-
 
 def write_holiday_cities(first_letter):
+    travel_notes = []
     visited_set = set()
-    visit_set = set()
-    visited_dict = {}
-    visit_dict = {}
-    # Получить данные из travel_notes.csv & записать в holiday.csv
-    with open('travel-notes.csv', 'r', newline='', encoding='utf8') as file_read, open('holiday.csv', 'w',
-                                                                                       encoding='utf8') as file_out:
-        # создаем ридер для входного файла
-        reader = csv.reader(file_read)
+    wish_set = set()
 
-        # записываем данные в holiday.csv
-        writer = csv.writer(file_out)
+    # Получить данные из travel_notes.csv & записать в holiday.csv
+    with (open('travel-notes.csv', 'r', newline='', encoding='utf8') as file_read):
+        reader = csv.reader(file_read)  # ридер для входного файла
 
         for row in reader:
 
-            if row[0].startswith(first_letter):
+            if row[0].startswith(first_letter):  # условие поиска по первой букве слова
                 pattern = r"\w+(?:(?:[^;]*\[[^][]*])+[^;]*|[^;']+)"
-                travel_notes.append(row)
+                travel_notes.append(row)  # добавляем соответсвующие столбцы в список
 
-                cities = re.findall(pattern, row[1])
+                cities = re.findall(pattern, row[1])  # столбец с посещенными городами добавляем в список
                 for city in cities:
                     visited_set.add(city)
-                visited_dict[row[0]] = cities
+                # print(visited_set)
 
-                cities = re.findall(pattern, row[2])
+                cities = re.findall(pattern, row[2])  # столбец с городами где не были добавляем в список
                 for city in cities:
-                    visit_set.add(city)
-                visit_dict[row[0]] = cities
+                    wish_set.add(city)
+                # print(visit_set)
 
-        print(visited_dict)
-        print(visit_dict)
+    all_cities = visited_set.union(wish_set)  # все города для всех имен
+    not_been = (all_cities.difference(visited_set))  # где не были для всех имен
+    next_city = sorted(not_been)[0] if not_been else ''  # следующий город
+
+    # создаем файл для записи
+    with (open('holiday.csv', 'w', newline='', encoding='utf8') as file_out):
+        writer = csv.writer(file_out)  # врайтер для записи данных в holiday.csv
+
+        writer.writerow(['Уже были в: ', ', '.join(sorted(visited_set))])
+        writer.writerow(['Хотят побывать в: ', ', '.join(sorted(wish_set))])
+        writer.writerow(['Еще не были в: ', ', '.join(not_been)])
+        writer.writerow(['Следующий город: ', next_city])
 
 
-letter = "A"
-write = write_holiday_cities(letter)
+letter = "L"
+find_cities_by_letter_name = write_holiday_cities(letter)
